8e09a069753e7c0929dba12cf35966f6377945b9
oleg097 <oleg097097@gmail.com>
2019-11-20 22:44:14 +0200

    first commit

---
+++ src/main/java/test/AppControllers/RegistrationController.java
@@ -0,0 +1,42 @@
+package test.AppControllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import test.Entityes.Role;
+import test.Entityes.User;
+import test.Repositoryes.UserRepos;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserRepos userRepos;
+
+    // ммэппинг на страницу регистрации
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Model model) {
+        User userFromDb = userRepos.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
+            model.addAttribute("message", "User exists!");
+            return "registration";
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepos.save(user);
+
+        return "redirect:/login";
+    }
+}
